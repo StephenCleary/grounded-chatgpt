0f3bb92b82516c36cf5198b05e12b2658a639e43
Stephen Cleary <github.ourteddybear@xoxy.net>
2023-04-22 02:38:41 +0000

    Use Seq for logging.

--- server/Program.cs
+++ server/Program.cs
@@ -4,7 +4,7 @@ using server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddLogging(logs => logs.AddSeq("http://seq:5341"));
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
