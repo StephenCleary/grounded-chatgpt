04cc250345a71bd4a40e9262cf42a2d7cde2be9f
Stephen Cleary <github.ourteddybear@xoxy.net>
2023-04-22 03:58:05 +0000

    Add esclient, ResourceDownloader.

---
+++ server/Data/ResourceDownloader.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Data;
+
+public sealed class ResourceDownloader
+{
+    public ResourceDownloader(IWebHostEnvironment webHostEnvironment)
+    {
+        _scratchPath = Path.Combine(webHostEnvironment.ContentRootPath, "scratch");
+        Directory.CreateDirectory(_scratchPath);
+    }
+
+    private readonly string _scratchPath;
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,12 +1,15 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Elastic.Clients.Elasticsearch;
 using server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging(logs => logs.AddSeq("http://seq:5341"));
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSingleton(new ElasticsearchClient(new Uri("http://elasticsearch:9200")));
+builder.Services.AddSingleton<ResourceDownloader>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
