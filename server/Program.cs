3c97e08b4403fd06696b44fe1fe8efaefd0ad713
Stephen Cleary <github.ourteddybear@xoxy.net>
2023-04-22 02:21:34 +0000

    Remove https.

--- server/Program.cs
+++ server/Program.cs
@@ -11,16 +11,6 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
